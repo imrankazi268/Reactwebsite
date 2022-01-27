02a7028e867c2b3a722fb12928d9e0fd67882666
Imran Kazi <it.imran@podar.org>
2022-01-27 14:38:07 +0530

    about change

--- src/About/About.js
+++ src/About/About.js
@@ -4,7 +4,7 @@ class About extends Component {
     render() {
         return (
             <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
-                <div><h2>About Page</h2></div>
+                <div><h2>About My website Page</h2></div>
             </div>
         );
     }
